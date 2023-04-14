9f467dcacc7bc6507291381db27905fd8ce4bfc0
Rhys Paulino <rhys.paulino@vanarts.com>
2023-04-14 00:16:09 -0700

    Twitter project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
